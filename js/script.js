363a6b6eb4b743c09b92b298e2dc6c62d3d032ab
Max Unanian <Zeptorqyiad99@gmail.com>
2024-06-06 19:08:16 +0500

    Finished with all script

--- js/script.js
+++ js/script.js
@@ -23,3 +23,54 @@ showButton2.addEventListener('click', function () {
 $('body').on('click', '.mini-image', function () {
    $('#main').attr('src', $(this).attr('src'))
 })
+
+function changeColor() {
+   let checkbox = document.getElementById('imageCheckbox')
+   let image = document.getElementById('myImage')
+   if (checkbox.checked) {
+      image.style.filter = 'grayscale(100%)'
+   } else {
+      image.style.filter = 'grayscale(0%)'
+   }
+   let checkbox2 = document.getElementById('imageCheckbox2')
+   let image2 = document.getElementById('myImage2')
+   if (checkbox2.checked) {
+      image2.style.filter = 'grayscale(100%)'
+   } else {
+      image2.style.filter = 'grayscale(0%)'
+   }
+   let checkbox3 = document.getElementById('imageCheckbox3')
+   let image3 = document.getElementById('myImage3')
+   if (checkbox3.checked) {
+      image3.style.filter = 'grayscale(100%)'
+   } else {
+      image3.style.filter = 'grayscale(0%)'
+   }
+   let checkbox4 = document.getElementById('imageCheckbox4')
+   let image4 = document.getElementById('myImage4')
+   if (checkbox4.checked) {
+      image4.style.filter = 'grayscale(100%)'
+   } else {
+      image4.style.filter = 'grayscale(0%)'
+   }
+   let checkbox5 = document.getElementById('imageCheckbox5')
+   let image5 = document.getElementById('myImage5')
+   if (checkbox5.checked) {
+      image5.style.filter = 'grayscale(100%)'
+   } else {
+      image5.style.filter = 'grayscale(0%)'
+   }
+}
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+for (let i = 0; i < checkboxes.length; i++) {
+   checkboxes[i].addEventListener('click', function (e) {
+      if (e.target.checked) {
+         for (let j = 0; j < checkboxes.length; j++) {
+            if (checkboxes[j] !== e.target) {
+               checkboxes[j].checked = false
+            }
+         }
+      }
+   })
+}
